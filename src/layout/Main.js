f0395082d73a9f76c438530704d7ac565e92df77
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2023-05-02 00:18:05 +0600

    banner update

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from '../Pages/Shared/Header/Header';
-import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Container>
-                <Outlet></Outlet>
-            </Container>
+            <Outlet></Outlet>
         </div>
     );
 };
